570a5777a0b377e5e93ee84b6f1cc3a9414131d2
Poul Henriksen <POHE@zealand.dk>
2024-03-14 08:55:45 +0100

    sd

---
+++ AdapterPatternOpgave2/NewSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPatternOpgave2
+{
+    public class NewSystem
+    {
+        private IDataService dataService;
+
+        public NewSystem(IDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        public void GetData()
+        {
+            // Brug af det fælles interface til at hente data, uanset hvilken service der bruges
+            string data = dataService.RetrieveData();
+            Console.WriteLine("Data received: " + data);
+        }
+    }
+}
